62567c16294d7c8b1f6bc585f52e6ef1211a54f8
Nils <nidresch@googlemail.com>
2019-10-08 12:56:12 +0200

    Only return the css value, not full property

--- index.ts
+++ index.ts
@@ -331,10 +331,10 @@ class Solver {
     function fmt(idx: any, multiplier = 1) {
       return Math.round(filters[idx] * multiplier);
     }
-    return `filter: invert(${fmt(0)}%) sepia(${fmt(1)}%) saturate(${fmt(
+    return `invert(${fmt(0)}%) sepia(${fmt(1)}%) saturate(${fmt(
       2
     )}%) hue-rotate(${fmt(3, 3.6)}deg) brightness(${fmt(4)}%) contrast(${fmt(
       5
-    )}%);`;
+    )}%)`;
   }
 }
